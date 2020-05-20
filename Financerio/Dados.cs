84465406ae947d640dc4412b3ce1d1b3e29e7093
Iago Lucas <iago0905@hotmail.com>
2020-05-20 14:06:38 -0300

    Inicio do Porjeto

---
+++ Financerio/Dados.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Financerio
+{
+    class Dados
+    {
+        public int Codigo { get; set; }
+        public DateTime Data { get; set; }
+        public string Consumidor { get; set; }
+        public string Estabelecimento { get; set; }
+        public string Local { get; set; }
+        public string Descricao { get; set; }
+        public string FormaPagamento { get; set; }
+        public string TipoDeCartao { get; set; }
+        public int QuantidadeDeParcelas { get; set; }
+        public double ValorTotal { get; set; }
+
+        public Dados(int codigo, DateTime data, string consumidor)
+        {
+            Codigo = codigo;
+            Data = data;
+            Consumidor = consumidor;        }
+
+        public Dados(int codigo, string data, string nome)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return
+                Codigo
+                + ", "
+                + Data
+                + ", "
+                + Consumidor;
+        }
+
+    }
+}
---
+++ Financerio/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Financerio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // DECLARAÇÃO = INSTANCIAMENTO
+            List<Dados> list = new List<Dados>();
+
+
+            Console.WriteLine("Entre com a quantidade");
+            int n = int.Parse(Console.ReadLine());
+
+            int codigo = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                codigo = i + 1;
+
+                Console.Write("Data: ");
+                DateTime data = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Consumidor: ");
+                string consumidor = Console.ReadLine();
+
+                list.Add(new Dados(codigo, data, consumidor));
+            }
+                       
+            Console.WriteLine("");
+            foreach (Dados obj in list)
+            {
+                Console.WriteLine(obj);
+            }
+
+
+        }
+    }
+}
